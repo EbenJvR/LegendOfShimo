538f5ba4bb2ae2ef22eb549a87963b3e4f3e50ad
DanielJacobs94 <niel.jacobs94@gmail.com>
2015-05-23 20:09:28 +0200

    Ice Shard Modification And Snow Polygon Colliders Added
    Made Ice Shards inable to go through ground, and added polygon colliders
    to all snow objects in Stage1Level1

--- Assets/Script/Player/MovementMk2.cs
+++ Assets/Script/Player/MovementMk2.cs
@@ -6,12 +6,9 @@ public class MovementMk2 : MonoBehaviour {
 	// Use this for initialization
 	private Animator running;
 	BoxCollider2D playerCollider;
-	public float speed = 3f;
+	public float speed = 10f;
 	Rigidbody2D datRigidBody;
 	public bool isGrounded = false;
-	public Transform pointA;
-	public Transform pointB;
-	public LayerMask layerMask;
 	int currentJump = 0;
 	public float jumpForce = 10000f;
 	public float doubleJumpForce = 10000f;
@@ -124,6 +121,11 @@ public class MovementMk2 : MonoBehaviour {
 		}
 		checkFall = false;
 	}
+	void SetMovement(float other)
+	{
+		speed = other;
+	}
+
 }
 
 
