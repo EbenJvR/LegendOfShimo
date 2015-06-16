05239284cfee9b6c6909b2e930abf1ab83dd5e92
CJDumont <courtneydumont@gmail.com>
2015-06-17 00:55:07 +0200

    Rolling Object
    Rolling object will remove the player's health by 50 for now because we
    are working with fixed digits for now, the rolling object can't be
    really be pushed by the player and the code will only apply damage once
    for now and rolling object will spawn only once as well.

---
+++ Assets/Script/Rolling Objects/IsRollingObjectSpawn.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsRollingObjectSpawn : MonoBehaviour 
+{
+	
+	public bool spawnObject;
+	
+	public Object rollingObject;
+	
+	public Transform spawningPoint;
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player" && spawnObject == false) 
+		{
+			Debug.Log("Player has passed");
+			//spawningPoint.transform.position in Instantiate will get the position of where it needs to spawn.
+
+			Instantiate(rollingObject, spawningPoint.transform.position, Quaternion.identity);
+
+
+			spawnObject = true;
+		}
+	}
+
+
+	//Old code with raycast used.
+
+	//bool spawn;
+	//public LayerMask playerMask ;
+	//int layermask = 1 << 8;
+	// This would cast rays only against colliders in layer 8.
+
+
+	//RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.up, 6000, layermask);
+	
+	//if (hit.collider != null) 
+	//{
+	//Debug.Log("Player has passed");
+	//spawn = false;
+	//}
+
+}
---
+++ Assets/Script/Rolling Objects/ObjectDamage.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDamage : MonoBehaviour {
+
+	private Health healthScript;
+	private bool gotHit;
+
+	void Start () 
+	{
+		healthScript = (Health)FindObjectOfType (typeof(Health));
+		gotHit = false;
+	}
+	
+	void OnCollisionEnter2D(Collision2D other)
+	{
+
+		if(other.gameObject.tag == "Player" && gotHit == false)
+		{
+			Debug.Log("Touch player");
+			healthScript.Damage(50);
+			gotHit = true;
+		}
+	}
+}
